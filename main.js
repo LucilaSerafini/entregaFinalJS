e44a9e06629d8abb01af33f73da102440a0f9169
Lucila Serafini <lucilaserafinich@gmail.com>
2024-03-30 00:13:11 -0300

    JSupdate3

--- main.js
+++ main.js
@@ -5,106 +5,73 @@ const productos = [
         id:'Kingdomino',
         titulo: 'Kingdomino',
         imagen: './resources/kingdomino.png',
-        categoria: {
-            nombre: 'juegos',
-            id: 'Kingdomino'
-        },
         precio: 1500
     },
     {
         id:'Burako',
         titulo: 'Burako',
         imagen: './resources/burako.png',
-        categoria: {
-            nombre: 'juegos',
-            id: 'Burako'
-        },
         precio: 1500
     },
     {
         id:'Catan',
         titulo: 'Catan',
         imagen: './resources/catan.png',
-        categoria: {
-            nombre: 'juegos',
-            id: 'Catan'
-        },
         precio: 1500
     },
     {
         id:'Dables',
         titulo: 'Dables',
         imagen: './resources/dables.png',
-        categoria: {
-            nombre: 'juegos',
-            id: 'Dables'
-        },
         precio: 1500
     },
     {
         id:'DigaloConMemes',
         titulo: 'DigaloConMemes',
         imagen: './resources/digaloConMemes.png',
-        categoría: {
-            nombre: 'juegos',
-            id: 'DigaloConMemes'
-        },
         precio: 1500
     },
     {
         id:'Estanciero',
         titulo: 'Estanciero',
         imagen: './resources/estanciero.png',
-        categoría: {
-            nombre: 'juegos',
-            id: 'Estanciero'
-        },
         precio: 1500
     },
     {
         id:'Monopoly',
         titulo: 'Monopoly',
         imagen: './resources/monopoly.png',
-        categoria: {
-            nombre: 'juegos',
-            id: 'Monopoly'
-        },
         precio: 1500
     },
     {
         id:'Munchkin',
         titulo: 'Munchkin',
         imagen: './resources/munchkin.png',
-        categoria: {
-            nombre: 'juegos',
-            id: 'Munchkin'
-        },
         precio: 1500
     },
     {
         id:'Teg',
         titulo: 'Teg',
         imagen: './resources/teg.png',
-        categoria: {
-            nombre: 'juegos',
-            id: 'Teg'
-        },
         precio: 1500
     },
     {
         id:'Uno',
         titulo: 'Uno',
         imagen: './resources/uno.png',
-        categoria: {
-            nombre: 'juegos',
-            id: 'Uno'
-        },
         precio: 1500
     },
 
 ]
 
+//Linkeo con HTML 
  const contenedorProductos = document.querySelector('#contenedor-productos');
+ const botonesMenu = document.querySelectorAll('.boton-menu');
+ let botonesAgregarAlCarrito = document.querySelectorAll(".agregarAlCarrito");
+ const numeroCarrito = document.querySelector("#numero-carrito");
+
+
+
 
  function cargarProductos() {
     productos.forEach(producto => {
@@ -114,14 +81,71 @@ const productos = [
             <div class="cardStyle">
                 <h5 class="card-title">${producto.titulo}</h5>
                 <p class="card-text">$${producto.precio} </p>
-                <button class="card-btn agregarAlCarrito" data-id="${producto.id}">AGREGAR</button>
+                <button class="card-btn agregarAlCarrito" id="${producto.id}">AGREGAR</button>
             </div>
         `;
         contenedorProductos.append(div);
     });
+    actualizarBotonesAgregar();
+
+
 }
 
 cargarProductos();
+
+
+//Botones de Menú - se cambia la etiqueta active dependiendo de en qué botón se haga click
+botonesMenu.forEach(boton => {
+    boton.addEventListener('click', (e) => {
+    
+    botonesMenu.forEach(boton => boton.classList.remove('active'));    
+
+    e.currentTarget.classList.add('active');
+    })
+})
+
+//Agregar al carrito
+
+//Función para que cada vez que se carguen productos nuevos se creen los botones correspondientes
+
+function actualizarBotonesAgregar() {
+    botonesAgregarAlCarrito = document.querySelectorAll(".agregarAlCarrito");
+
+    botonesAgregarAlCarrito.forEach(boton => {
+        boton.addEventListener('click', agregarAlCarrito);
+    });
+}
+
+//Array para agregar al carrito los ítems que se seleccionen
+
+const productosAgregadosACarrito = [];
+
+function agregarAlCarrito(e) {
+    const idBoton = e.currentTarget.id;
+    const productoAgregado = productos.find(producto => producto.id === idBoton);
+
+    if(productosAgregadosACarrito.some(producto => producto.id === idBoton)) {
+        //hay que buscar sie l producto agregado ya existe en el carrito para no duplicarlo, sólo aumentar la cantidad
+        const index = productosAgregadosACarrito.findIndex(producto => producto.id === idBoton);
+        productosAgregadosACarrito[index].cantidad++;
+
+    } else {
+        productoAgregado.cantidad = 1;
+        productosAgregadosACarrito.push(productoAgregado);
+    }
+
+    actualizarNumeroCarrito();
+
+    //Local Storage del array del carrito para poder llevarlo a la página de carrito
+
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosAgregadosACarrito));
+}
+
+//Función para que se actualice el número del carrito en el index 
+function actualizarNumeroCarrito() {
+    const numeroCarritoMenu = productosAgregadosACarrito.reduce((acc, producto)=> acc + producto.cantidad, 0);
+    numeroCarrito.innerText = numeroCarritoMenu;
+}
 
 
 
